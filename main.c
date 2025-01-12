e6e9d8528cce9adec2cc71f337af47a7bfe2491d
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2025-01-12 17:22:55 +0100

    Empty string working

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-
-int main(void)
+int main(int argc, char* argv[])
 {
+	if (argc != 3)
+	{
+		printf("Usage: ./%s <pattern> <filetosearch>", argv[0]);
+		return 1;
+	}
 
-}
+	const char* pattern = argv[1];
+	const char* filetosearch = argv[2];
 
-void grep(const char* pattern, const char* filename)
-{
+	printf("%s\n", pattern);        
+	printf("%s\n", filetosearch);  
 
+	FILE* file = fopen(filetosearch, "r");
+
+	if (file == NULL) {
+		perror("Error opening file");
+		return 1;
+	}
+
+	if (strcmp(pattern, "") == 0 ) {
+		char line[256];
+		while (fgets(line, sizeof(line), file)) {
+			printf("%s", line);
+		}
+	}
+
+	fclose(file);
+	return 0;
 }
