da2b50db47fe9cff0efa9147b3bb82b1f58a97ce
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2025-01-12 18:06:40 +0100

    Basic functionality

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
 	if (argc != 3)
 	{
-		printf("Usage: ./%s <pattern> <filetosearch>", argv[0]);
+		printf("Usage: ./main.exe <pattern> <filetosearch>", argv[0]);
 		return 1;
 	}
 
@@ -21,13 +21,25 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	if (strcmp(pattern, "") == 0 ) {
-		char line[256];
+	char line[256];
+	int lineNumber = 0;
+
+	if (strcmp(pattern, "") == 0) {
 		while (fgets(line, sizeof(line), file)) {
 			printf("%s", line);
 		}
 	}
 
+	else {
+		while (fgets(line, sizeof(line), file)) {
+			lineNumber++;
+
+			if (strstr(line, pattern) != NULL) {
+				printf("Line %d: %s", lineNumber, line);
+			}
+		}
+	}
+		
 	fclose(file);
 	return 0;
 }
