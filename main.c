c403e292470cb96c8758f02c28525f32c910db00
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2025-01-15 11:50:29 +0100

    -n working

--- main.c
+++ main.c
@@ -2,22 +2,47 @@
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
-	{
-		printf("Usage: ./main.exe <pattern> <filetosearch>", argv[0]);
-		return 1;
+	int case_insensitive = 0;
+	int inverted_match = 0;
+	int line_numbers = 0;
+
+	const char* pattern = NULL;
+	const char* filename = NULL;
+
+	for (int i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			if (strstr(argv[i], "i")) {
+				case_insensitive = 1;
+			}
+			if (strstr(argv[i], "v")) {
+				inverted_match = 1;
+			}
+			if (strstr(argv[i], "n")) {
+				line_numbers = 1;
+			}
+		}
+		else if (pattern == NULL) {
+			pattern = argv[i];
+		}
+		else {
+			filename = argv[i];
+		}
 	}
 
-	const char* pattern = argv[1];
-	const char* filetosearch = argv[2];
+	printf("Flags:\n");
+	printf("  Case Insensitive (-i): %d\n", case_insensitive);
+	printf("  Inverted Match (-v): %d\n", inverted_match);
+	printf("  Line Numbers (-n): %d\n", line_numbers);
+	printf("\n");
 
-	printf("%s\n", pattern);        
-	printf("%s\n", filetosearch);  
+	printf("Pattern: %s\n", pattern ? pattern : "(null)");
+	printf("Filename: %s\n", filename ? filename : "(null)");
 
-	FILE* file = fopen(filetosearch, "r");
+
+	FILE* file = fopen(filename, "r");
 
 	if (file == NULL) {
-		perror("Error opening file");
+		perror("Error opening	file");
 		return 1;
 	}
 
@@ -35,11 +60,17 @@ int main(int argc, char* argv[])
 			lineNumber++;
 
 			if (strstr(line, pattern) != NULL) {
-				printf("Line %d: %s", lineNumber, line);
+				if (line_numbers)
+				{
+					printf("Line %d: %s", lineNumber, line);
+				}
+				else {
+					printf("%s", line);
+				};
 			}
 		}
+
+		fclose(file);
+		return 0;
 	}
-		
-	fclose(file);
-	return 0;
 }
